b5e1023731b90563353704d03edc78cda31fa4ba
SOMNIUMWAVE <66190479+somniumwave@users.noreply.github.com>
2022-04-30 20:11:36 -0700

    Integrate WebGL player

--- pages/enter.js
+++ pages/enter.js
@@ -1,20 +1,32 @@
 import Head from'next/head'
 import Link from 'next/link'
+import Unity, { UnityContext } from "react-unity-webgl"
+
+const unityContext = new UnityContext({
+  loaderUrl: "Build/SW1.loader.js",
+  dataUrl: "Build/SW1.data.unityweb",
+  frameworkUrl: "Build/SW1.framework.js.unityweb",
+  codeUrl: "Build/SW1.wasm.unityweb",
+});
 
 export default function Home() {
+  
   return (
-    <div className="flex flex-col h-full bg-banner bg-cover bg-black">
+    <div className="flex flex-col h-full bg-banner bg-cover ">
       <div className="flex flex-col h-full bg-black bg-opacity-20">
 
         <Head>
           <title>ENTER // Somniumwave - Official Website</title>
           <meta name='keywords' content='web development, programming' />
         </Head>
-
+ 
         <div className="m-auto shadow-xl">
-          <button className="w-[400px] font-bold text-4xl bg-[#000000] bg-opacity-80 text-white p-4 rounded-2xl">
-            WEBGL GAME HERE
-          </button>
+          <div className="">
+            <Unity
+              unityContext={unityContext}
+              style={{ width: "1280px", height: "720px"}}
+            />
+          </div>
         </div>
 
       </div>
